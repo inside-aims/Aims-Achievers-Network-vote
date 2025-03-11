11eaf7770a275fbb4e20f9410d3ea14063e919fd
khonello <rkoomson777@gmail.com>
2025-03-11 15:32:04 +0000

    setup supabase as backend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,29 @@
 "use client"
-
+import { useEffect, useState } from "react"
+import { createClient } from '@/config/client'
 import { motion } from "framer-motion"
 import CategoryGrid from "@/components/categories/CategoryGrid"
-import { categories } from "@/data/categories"
+
+const supabase = createClient()
 
 export default function Home() {
+  const [categories, setCategories] = useState<any[]>([])
+
+  useEffect(() => {
+
+    (async () => {
+        const { data, error } = await supabase.from("category").select("*")
+        if (error) {
+          console.error("Error fetching categories:", error)
+        } else {
+          setCategories(data || [])
+        }
+      }
+    )()
+  }, [])
+
   return (
-<>
-<div className="min-h-screen bg-zinc-900">
+    <div className="min-h-screen bg-zinc-900">
       <main className="container mx-auto px-4 py-16">
         <motion.h1
           initial={{ opacity: 0, y: -20 }}
@@ -20,6 +36,5 @@ export default function Home() {
         <CategoryGrid categories={categories} />
       </main>
     </div>
-</>
-  );
+  )
 }
